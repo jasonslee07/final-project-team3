665543411556822976628a55d465354d226edc60
Jason Lee <jasonlee131045@gmail.com>
2026-04-06 21:56:06 -0400

    created pages folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import LoginPage from "./components/LoginPage";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
 import PageNotFound from "./components/PageNotFound";
 
 function App() {
