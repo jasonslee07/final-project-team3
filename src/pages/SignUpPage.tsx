2a8c76d6a87674d9d238a3c2c64b54f38f350d5d
Yuvan Adarsh Jagannathan <yuvanadarshj@gmail.com>
2026-04-08 10:32:35 -0400

    fix: added the vendor/client functionality

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import Navbar from "../components/Navbar";
 
 const SignUpPage = () => {
+  const [isClient, setIsClient] = useState<boolean>(true);
+  const [isVendor, setIsVendor] = useState<boolean>(false);
+
+  function handleClick() {
+    setIsClient(!isClient);
+    setIsVendor(!isVendor);
+  }
   return (
     <>
       <div className="flex flex-col h-screen">
@@ -10,12 +18,16 @@ const SignUpPage = () => {
           <div className="bg-linear-to-b from-[#EAECDC] to-[#D3D6BA] w-full border-t-8 flex-1 border-[#E2725B] flex flex-col pt-10 items-center">
             <form action="" className="flex flex-col justify-center items-center w-90 gap-8">
               <div className="flex gap-10">
-                <button className="text-2xl px-8 py-2 bg-white rounded-2xl">Client</button>
-                <button className="text-2xl px-8 py-2 bg-white rounded-2xl">Vendor</button>
+                <button type="button" className={`text-2xl px-8 py-2 rounded-2xl ${isClient ? "bg-[#A8B897]" : "bg-white"}`} onClick={handleClick}>
+                  Client
+                </button>
+                <button type="button" className={`text-2xl px-8 py-2 rounded-2xl ${isVendor ? "bg-[#A8B897]" : "bg-white"}`} onClick={handleClick}>
+                  Vendor
+                </button>
               </div>
               <p className="text-xl text-[#40532D]">I want to join as a...</p>
-              <input type="text" placeholder="First Name" required className="py-3 px-4 border w-full bg-white rounded-md text-[#D3D6BA]" />
-              <input type="text" placeholder="Last Name" required className="py-3 px-4 border w-full bg-white rounded-md text-[#D3D6BA]" />
+              <input type="text" placeholder="First Name" required className="py-3 px-4 border w-full bg-white rounded-md text-[#D3D6BA] focus:text-[#AABA99]" />
+              <input type="text" placeholder="Last Name" required className="py-3 px-4 border w-full bg-white rounded-md text-[#D3D6BA] focus:text-[#AABA99]" />
               <input type="email" placeholder="Email" required className="py-3 px-4 border w-full bg-white rounded-md text-[#D3D6BA]" />
               <input type="password" placeholder="Password" required className="py-3 px-4 border w-full bg-white rounded-md text-[#D3D6BA]" />
               <button type="submit" className="py-3 bg-[#E2725C] text-white rounded-md w-full">
