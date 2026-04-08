75c1b41be27e970c5ace3548ead7f15f7f807512
Yuvan Adarsh Jagannathan <yuvanadarshj@gmail.com>
2026-04-08 01:25:16 -0400

    feat: Login Form UI Completed + Forgot password routing

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import ClientProfile from "./pages/ClientProfile";
 import VendorProfile from "./pages/VendorProfile";
 import ItemPage from "./pages/ItemPage";
 import SignUpPage from "./pages/SignUpPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           <Route path="/login" element={<LoginPage />}></Route>
           <Route path="/sign-up" element={<SignUpPage />}></Route>
+          <Route path="/forgot-password" element={<ForgotPasswordPage />}></Route>
           <Route path="*" element={<PageNotFound />}></Route>
           <Route path="/client-profile" element={<ClientProfile />}></Route>
           <Route path="/vendor-profile" element={<VendorProfile />}></Route>
---
+++ src/pages/ForgotPasswordPage.tsx
@@ -0,0 +1,12 @@
+// import Navbar from "../components/Navbar";
+
+const ForgotPasswordPage = () => {
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-2xl font-bold flex justify-center">I Forgot My Password!</h1>
+    </>
+  );
+};
+
+export default ForgotPasswordPage;
